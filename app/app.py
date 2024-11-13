72eac0534164a5f39c1ce0555013041ce6021b76
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-11-14 00:27:45 +0800

    session management configuration, registration route implemented

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_session import Session
+from database.db import get_db_connection
 
 app = Flask(__name__)
+
+# session management config
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 @app.route('/')
 # current task is to just display user information associated to current session
@@ -8,7 +15,7 @@ def index():
     return render_template('index.html')
 
 # if login info matches to an account in the db, user logs in
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     pass
 
@@ -18,9 +25,32 @@ def logout():
     pass
 
 # create a user object and redirect to login
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    if request.method == 'POST':
+        email = request.form.get('email')
+        passkey = request.form.get('passkey')
+        lname = request.form.get('lname')
+        fname = request.form.get('fname')
+        middle_initial = request.form.get('middle_initial')
+        birth_date = request.form.get('birth_date')
+        sex = request.form.get('sex')
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        registration_query = """
+            INSERT INTO passenger(email, passkey, lname, fname, middle_initial, birth_date, sex)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(registration_query, (email, passkey, lname,
+                                            fname, middle_initial, birth_date, sex))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return redirect('/login')
+    return render_template('sessionmgt/register.html')
 
 # set app to debug mode makes it so that you can serve the application by running `python app.py` via terminal
 if __name__ == '__main__':
