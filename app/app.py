ae359db7f30aa09a5621c09d3a33c238674043a2
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-11-13 23:34:25 +0800

    route skeletons for session management

--- app/app.py
+++ app/app.py
@@ -3,8 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+# current task is to just display user information associated to current session
 def index():
     return render_template('index.html')
+
+# if login info matches to an account in the db, user logs in
+@app.route('/login')
+def login():
+    pass
+
+# clear session cookie
+@app.route('/logout')
+def logout():
+    pass
+
+# create a user object and redirect to login
+@app.route('/register')
+def register():
+    pass
 
 # set app to debug mode makes it so that you can serve the application by running `python app.py` via terminal
 if __name__ == '__main__':
